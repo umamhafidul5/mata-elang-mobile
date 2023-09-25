09a2022fd5d7096433405ce246ea8620dc0f94cb
umamhafidul5 <umamhafidul5@gmail.com>
2023-09-25 11:10:44 +0700

    initial commit

---
+++ src/main/java/com/indocyber/mataelangmobile/MataElangMobileApplication.java
@@ -0,0 +1,13 @@
+package com.indocyber.mataelangmobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MataElangMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MataElangMobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/indocyber/mataelangmobile/MataElangMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.indocyber.mataelangmobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MataElangMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
